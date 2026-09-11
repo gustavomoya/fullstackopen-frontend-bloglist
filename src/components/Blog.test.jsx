947d3abebfbe8885f33a899f68e639808fe6e65b
Gustavo <gustavo.moya@geniusreferrals.com>
2026-09-11 09:28:35 -0400

    updating tests

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -4,133 +4,133 @@ import { MemoryRouter } from 'react-router-dom'
 import Blog from './Blog'
 
 test('renders content unauthenticated users', () => {
-  const blog = {
-    id: '123456',
-    title: 'Component testing is done with react-testing-library',
-    author: 'Jane Doe',
-    url: 'https://example.com/blog',
-    likes: 10,
-    user: {
-      id: '123',
-      name: 'John Doe'
+    const blog = {
+        id: '123456',
+        title: 'Component testing is done with react-testing-library',
+        author: 'Jane Doe',
+        url: 'https://example.com/blog',
+        likes: 10,
+        user: {
+            id: '123',
+            name: 'John Doe'
+        }
     }
-  }
 
-  render(
-      <MemoryRouter>
-        <Blog blog={blog} />
-      </MemoryRouter>
-  )
+    render(
+        <MemoryRouter>
+            <Blog blog={blog}/>
+        </MemoryRouter>
+    )
 
-  const element = screen.getByText(
-    'Component testing is done with react-testing-library', { exact: false }
-  )
-  expect(element).toBeDefined()
+    const element = screen.getByText(
+        'Component testing is done with react-testing-library', { exact: false }
+    )
+    expect(element).toBeDefined()
 
-  expect(screen.getByText('https://example.com/blog')).toBeVisible()
-  expect(screen.getByText('10')).toBeVisible()
+    expect(screen.getByText('https://example.com/blog')).toBeVisible()
+    expect(screen.getByText('10 likes')).toBeVisible()
 
-  expect(screen.queryByText('Like')).toBeNull()
-  expect(screen.queryByText('Remove')).toBeNull()
+    expect(screen.queryByText('Like')).toBeNull()
+    expect(screen.queryByText('Remove')).toBeNull()
 
 })
 
 test('render content to a user who is not the owner', () => {
-  const blog = {
-    id: '123456',
-    title: 'Component testing is done with react-testing-library',
-    author: 'Jane Doe',
-    url: 'https://example.com/blog',
-    likes: 10,
-    user: {
-      id: '123',
-      name: 'John Doe'
+    const blog = {
+        id: '123456',
+        title: 'Component testing is done with react-testing-library',
+        author: 'Jane Doe',
+        url: 'https://example.com/blog',
+        likes: 10,
+        user: {
+            id: '123',
+            name: 'John Doe'
+        }
     }
-  }
 
-  const user = {
-    id: '456',
-    name: 'Jane Doe'
-  }
+    const user = {
+        id: '456',
+        name: 'Jane Doe'
+    }
 
-   render(
-       <MemoryRouter>
-         <Blog blog={blog} user={user} />
-       </MemoryRouter>
-   )
+    render(
+        <MemoryRouter>
+            <Blog blog={blog} user={user}/>
+        </MemoryRouter>
+    )
 
-  expect(screen.getByText('https://example.com/blog')).toBeVisible()
-  expect(screen.getByText('10')).toBeVisible()
-  expect(screen.getByText('Like')).toBeVisible()
+    expect(screen.getByText('https://example.com/blog')).toBeVisible()
+    expect(screen.getByText('10 likes')).toBeVisible()
+    expect(screen.getByText('Like')).toBeVisible()
 
-  expect(screen.queryByText('Remove')).toBeNull()
+    expect(screen.queryByText('Remove')).toBeNull()
 })
 
 test('renders content to authenticated owner users', () => {
-  const blog = {
-    id: '123456',
-    title: 'Component testing is done with react-testing-library',
-    author: 'Jane Doe',
-    url: 'https://example.com/blog',
-    likes: 10,
-    user: {
-      id: '123',
-      name: 'John Doe'
+    const blog = {
+        id: '123456',
+        title: 'Component testing is done with react-testing-library',
+        author: 'Jane Doe',
+        url: 'https://example.com/blog',
+        likes: 10,
+        user: {
+            id: '123',
+            name: 'John Doe'
+        }
     }
-  }
 
-  const user = {
-    id: '123',
-    name: 'John Doe'
-  }
+    const user = {
+        id: '123',
+        name: 'John Doe'
+    }
 
-  render(
-      <MemoryRouter>
-        <Blog blog={blog} user={user} />
-      </MemoryRouter>
-  )
+    render(
+        <MemoryRouter>
+            <Blog blog={blog} user={user}/>
+        </MemoryRouter>
+    )
 
-  const element = screen.getByText(
-      'Component testing is done with react-testing-library', { exact: false }
-  )
-  expect(element).toBeDefined()
+    const element = screen.getByText(
+        'Component testing is done with react-testing-library', { exact: false }
+    )
+    expect(element).toBeDefined()
 
-  expect(screen.getByText('https://example.com/blog')).toBeVisible()
-  expect(screen.getByText('10')).toBeVisible()
-  expect(screen.getByText('Like')).toBeVisible()
-  expect(screen.getByText('Remove')).toBeVisible()
+    expect(screen.getByText('https://example.com/blog')).toBeVisible()
+    expect(screen.getByText('10 likes')).toBeVisible()
+    expect(screen.getByText('Like')).toBeVisible()
+    expect(screen.getByText('Remove')).toBeVisible()
 })
 
 test('clicking the button calls event handler twice', async () => {
-  const blog = {
-    id: '123456',
-    title: 'Component testing is done with react-testing-library',
-    author: 'Jane Doe',
-    url: 'https://example.com/blog',
-    likes: 10,
-    user: {
-      id: '123',
-      name: 'John Doe'
+    const blog = {
+        id: '123456',
+        title: 'Component testing is done with react-testing-library',
+        author: 'Jane Doe',
+        url: 'https://example.com/blog',
+        likes: 10,
+        user: {
+            id: '123',
+            name: 'John Doe'
+        }
     }
-  }
 
-  const user = {
-    id: '123',
-    name: 'John Doe'
-  }
+    const user = {
+        id: '123',
+        name: 'John Doe'
+    }
 
-  const mockHandler = vi.fn()
+    const mockHandler = vi.fn()
 
-  const { container } = render(
-      <MemoryRouter>
-        <Blog blog={blog} user={user} updateBlog={mockHandler}/>
-      </MemoryRouter>
-  )
+    const { container } = render(
+        <MemoryRouter>
+            <Blog blog={blog} user={user} updateBlog={mockHandler}/>
+        </MemoryRouter>
+    )
 
-  const uEvent = userEvent.setup()
-  const button = container.querySelector('.btn-add-like')
-  await uEvent.click(button)
-  await uEvent.click(button)
+    const uEvent = userEvent.setup()
+    const button = container.querySelector('.btn-add-like')
+    await uEvent.click(button)
+    await uEvent.click(button)
 
-  expect(mockHandler.mock.calls).toHaveLength(2)
+    expect(mockHandler.mock.calls).toHaveLength(2)
 })
--- src/components/BlogForm.test.jsx
+++ src/components/BlogForm.test.jsx
@@ -4,35 +4,35 @@ import { MemoryRouter } from 'react-router-dom'
 import BlogForm from './BlogForm.jsx'
 
 test('check form action', async () => {
-  const user = userEvent.setup()
+    const user = userEvent.setup()
 
-  const handleAddBlog = vi.fn()
+    const handleAddBlog = vi.fn()
 
-  render(
-    <MemoryRouter>
-        <BlogForm  createBlog={handleAddBlog} />
-    </MemoryRouter>
-  )
+    render(
+        <MemoryRouter>
+            <BlogForm createBlog={handleAddBlog}/>
+        </MemoryRouter>
+    )
 
-  const titleInput = screen.getByPlaceholderText('enter the title')
-  const authorInput = screen.getByPlaceholderText('enter the author')
-  const urlInput = screen.getByPlaceholderText('enter the url')
-  const likesInput = screen.getByPlaceholderText('enter the number of likes')
+    const titleInput = screen.getByPlaceholderText('enter the title')
+    const authorInput = screen.getByPlaceholderText('enter the author')
+    const urlInput = screen.getByPlaceholderText('enter the url')
+    const likesInput = screen.getByPlaceholderText('enter the number of likes')
 
-  const createButton = screen.getByText('Create')
+    const createButton = screen.getByText('Create')
 
-  await user.type(titleInput, 'Component testing is done with react-testing-library')
-  await user.type(authorInput, 'John Doe')
-  await user.type(urlInput, 'https://example.com/blog')
-  await user.type(likesInput, '10')
+    await user.type(titleInput, 'Component testing is done with react-testing-library')
+    await user.type(authorInput, 'John Doe')
+    await user.type(urlInput, 'https://example.com/blog')
+    await user.type(likesInput, '10')
 
-  await user.click(createButton)
+    await user.click(createButton)
 
-  expect(handleAddBlog.mock.calls).toHaveLength(1)
+    expect(handleAddBlog.mock.calls).toHaveLength(1)
 
-  expect(handleAddBlog.mock.calls[0][0].title).toBe('Component testing is done with react-testing-library')
-  expect(handleAddBlog.mock.calls[0][0].author).toBe('John Doe')
-  expect(handleAddBlog.mock.calls[0][0].url).toBe('https://example.com/blog')
-  expect(handleAddBlog.mock.calls[0][0].likes).toBe('10')
+    expect(handleAddBlog.mock.calls[0][0].title).toBe('Component testing is done with react-testing-library')
+    expect(handleAddBlog.mock.calls[0][0].author).toBe('John Doe')
+    expect(handleAddBlog.mock.calls[0][0].url).toBe('https://example.com/blog')
+    expect(handleAddBlog.mock.calls[0][0].likes).toBe('10')
 
 })
